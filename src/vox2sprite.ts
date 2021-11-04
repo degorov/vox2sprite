17e48614541e0ef21f3d13cfa582e4dfe9ea807d
Dmitry Egorov <mail@degorov.com>
2021-11-04 15:41:20 +0300

    Proper command-line interface

--- src/vox2sprite.ts
+++ src/vox2sprite.ts
@@ -1,43 +1,18 @@
 #!/usr/bin/env node
 
+import parseArgs from 'minimist';
 import vox2sprite from './spritesheet';
 
-const files = [
-  '7-7-7.vox',
-  '7-7-8.vox',
-  '7-8-7.vox',
-  '7-8-8.vox',
-  '8-7-7.vox',
-  '8-7-8.vox',
-  '8-8-7.vox',
-  '8-8-8.vox',
-  'christmas_scene.vox',
-  'chr_knight.vox',
-  'chr_old.vox',
-  'chr_rain.vox',
-  'chr_sword.vox',
-  'horse.vox',
-  'menger.vox',
-  'monu0.vox',
-  'monu10.vox',
-  'monu16.vox',
-  'monu1.vox',
-  'monu2.vox',
-  'monu3.vox',
-  'monu4.vox',
-  'monu5.vox',
-  'monu6.vox',
-  'monu6-without-water.vox',
-  'monu7.vox',
-  'monu8.vox',
-  'monu8-without-water.vox',
-  'monu9.vox',
-  'red_booth_solid.vox',
-  'street_scene.vox',
-  'tiletest.vox',
-  'treehouse.vox',
-];
+const {
+  _: [inFile, outFile],
+} = parseArgs(process.argv.slice(2));
 
-for (const fileName of files) {
-  vox2sprite(`./vox/${fileName}`, `./out/${fileName}.png`);
+if (inFile && outFile) {
+  try {
+    vox2sprite(inFile, outFile);
+  } catch (err) {
+    console.log((err as Error).message);
+  }
+} else {
+  console.info('Usage: vox2sprite voxfile.vox pngfile.png');
 }
